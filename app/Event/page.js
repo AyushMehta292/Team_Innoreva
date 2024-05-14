a3e25ea8985172648ac7fe5441a730782a23bc4f
Ayush_Mehta <2021ugee076@nitjsr.ac.in>
2024-05-14 19:11:20 +0530

    suspense added in /Event/page.js

--- app/Event/page.js
+++ app/Event/page.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import { builder, client } from "@/Helper/context";
+import { Suspense } from "react";
 
 const page = () => {
   const pathname = usePathname();
@@ -38,70 +39,69 @@ const page = () => {
   useEffect(() => {
     // console.log("curevent ", typeof curevent);
     if (Object.keys(curevent).length) console.log("curevent ", curevent);
-    if (Object.keys(curevent).length)
-      console.log(
-        "images ",
-      );
+    if (Object.keys(curevent).length) console.log("images ");
   }, [curevent]);
 
   return (
     <>
-      {Object.keys(curevent).length && (
-        <div className="w-full md:h-screen bg-[#000016] text-white">
-          <div className="flex h-full w-full">
-            <div className="w-[63%] h-fit flex flex-col m-24 mr-16 justify-center gap-4 text-left">
-              <div className=" font-[Archivo] font-light text-[#9B9B9B]">
-                {curevent.eventFrom}
-                {curevent.eventTo ? `- ${curevent.eventTo}` : ""}
-              </div>
-              <div className=" font-[Antonio] font-extrabold text-8xl leading-[7rem] ">
-                {curevent.eventName}
-              </div>
-              <div className=" font-[Anybody] text-2xl mb-6">
-                {curevent.collaborationWith
-                  ? `IN COLLABORATION WITH ${curevent.collaborationWith}`
-                  : ""}
-              </div>
-              <div className=" flex flex-col gap-4  items-stretch font-[Archivo] font-[600]">
-                <div className=" whitespace-pre-line">
-                  {curevent.highlights}
+      <Suspense>
+        {Object.keys(curevent).length && (
+          <div className="w-full md:h-screen bg-[#000016] text-white">
+            <div className="flex h-full w-full">
+              <div className="w-[63%] h-fit flex flex-col m-24 mr-16 justify-center gap-4 text-left">
+                <div className=" font-[Archivo] font-light text-[#9B9B9B]">
+                  {curevent.eventFrom}
+                  {curevent.eventTo ? `- ${curevent.eventTo}` : ""}
+                </div>
+                <div className=" font-[Antonio] font-extrabold text-8xl leading-[7rem] ">
+                  {curevent.eventName}
+                </div>
+                <div className=" font-[Anybody] text-2xl mb-6">
+                  {curevent.collaborationWith
+                    ? `IN COLLABORATION WITH ${curevent.collaborationWith}`
+                    : ""}
+                </div>
+                <div className=" flex flex-col gap-4  items-stretch font-[Archivo] font-[600]">
+                  <div className=" whitespace-pre-line">
+                    {curevent.highlights}
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className=" my-2  border-2 h-auto "></div>
-            <div className=" w-[33%] h-auto mr-24 my-auto ml-5 flex flex-col justify-center gap-4 ">
-              {curevent.images && (
-                <>
-                  <div className=" w-full h-[40%] flex justify-center items-center">
-                    <img
-                      src={`${urlFor(curevent.images[0].image)
-                        .width(500)
-                        .url()}`}
-                      alt=""
-                      className=" object-contain h-auto w-64 object-center  "
-                    />
-                  </div>
-                  <div className=" w-full h-fit text-center px-auto">
-                    {curevent.images[0].imageCaption}
-                  </div>
-                  <div className=" w-full h-[40%]   flex justify-center items-center">
-                    <img
-                      src={`${urlFor(curevent.images[1].image)
-                        .width(500)
-                        .url()}`}
-                      alt=""
-                      className=" object-contain h-auto w-64 object-center  "
-                    />
-                  </div>
-                  <div className=" w-full h-fit text-center  px-4">
-                    {curevent.images[1].imageCaption}
-                  </div>
-                </>
-              )}
+              <div className=" my-2  border-2 h-auto "></div>
+              <div className=" w-[33%] h-auto mr-24 my-auto ml-5 flex flex-col justify-center gap-4 ">
+                {curevent.images && (
+                  <>
+                    <div className=" w-full h-[40%] flex justify-center items-center">
+                      <img
+                        src={`${urlFor(curevent.images[0].image)
+                          .width(500)
+                          .url()}`}
+                        alt=""
+                        className=" object-contain h-auto w-64 object-center  "
+                      />
+                    </div>
+                    <div className=" w-full h-fit text-center px-auto">
+                      {curevent.images[0].imageCaption}
+                    </div>
+                    <div className=" w-full h-[40%]   flex justify-center items-center">
+                      <img
+                        src={`${urlFor(curevent.images[1].image)
+                          .width(500)
+                          .url()}`}
+                        alt=""
+                        className=" object-contain h-auto w-64 object-center  "
+                      />
+                    </div>
+                    <div className=" w-full h-fit text-center  px-4">
+                      {curevent.images[1].imageCaption}
+                    </div>
+                  </>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
+      </Suspense>
     </>
   );
 };
