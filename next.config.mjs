35f5718d8528fdd23a9fb38b0efed5898429bc73
Ayush_Mehta <2021ugee076@nitjsr.ac.in>
2024-05-24 22:53:25 +0530

    serverMinification turned false 2

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,18 @@ const nextConfig = {
       taint: true,
       serverMinification: false,
     },
+    compress: false,
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+      if (!dev) {
+        // Ensure that other optimization settings are preserved
+        config.optimization = {
+          ...config.optimization,
+          minimize: false,
+        };
+      }
+      // Important: return the modified config
+      return config;
+    },
 };
 
 export default nextConfig;
