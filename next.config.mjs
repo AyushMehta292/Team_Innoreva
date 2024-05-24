16d5b0cdb2cf8fa20a216c5058fba4d28c5e8390
Ayush_Mehta <2021ugee076@nitjsr.ac.in>
2024-05-24 23:25:41 +0530

    edited next config again

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,67 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: "https",
+//           hostname: "cdn.sanity.io",
+//         },
+//       ],
+//     },
+//     experimental: {
+//       taint: true,
+//       serverMinification: false,
+//     },
+//     compress: false,
+//     webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+//       if (!dev) {
+//         // Ensure that other optimization settings are preserved
+//         config.optimization = {
+//           ...config.optimization,
+//           config.optimization.minimizer: [],
+//         };
+//       }
+//       // Important: return the modified config
+//       return config;
+//     },
+//     swcMinify: false,
+// };
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "cdn.sanity.io",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+  compress: false, // Disable built-in compression
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+    if (!dev) {
+      // Disable minification and other optimizations in production
+      config.optimization.minimize = false;
+      config.optimization.splitChunks = {
+        chunks: 'async',
+        minSize: 0,
+        minChunks: 1,
+        cacheGroups: {
+          default: false,
+          vendors: false,
         },
-      ],
-    },
-    experimental: {
-      taint: true,
-      serverMinification: false,
-    },
-    compress: false,
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-      if (!dev) {
-        // Ensure that other optimization settings are preserved
-        config.optimization = {
-          ...config.optimization,
-          minimize: false,
-        };
-      }
-      // Important: return the modified config
-      return config;
-    },
+      };
+      // Remove TerserPlugin to avoid JS minification
+      config.optimization.minimizer = config.optimization.minimizer.filter(
+        (plugin) => plugin.constructor.name !== 'TerserPlugin'
+      );
+    }
+    // Important: return the modified config
+    return config;
+  },
 };
 
 export default nextConfig;
-
